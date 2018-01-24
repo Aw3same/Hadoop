8de2113c39930c634b7d7409dddd9d2e447fa285
Daniel <dvillaj@gmail.com>
2018-01-24 04:27:01 +0100

    Primera iteración

--- sentiment/src/main/java/Reduce.java
+++ sentiment/src/main/java/Reduce.java
@@ -1,23 +1,20 @@
-package org.myorg;
-
 import java.io.IOException;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
-public class Reduce extends 
+public class Reduce extends
 	Reducer<Text, IntWritable, Text, IntWritable> {
-@Override
+	@Override
 	public void reduce(Text word, Iterable<IntWritable> instances, Context context)
-			throws IOException, InterruptedException
-	{
+	throws IOException, InterruptedException {
 		int sum = 0;
 
 		// Sum up the instances of the current word.
 		for (IntWritable instance : instances) {
 			sum += instance.get();
 		}
-		
+
 		// Write the word and count to output.
 		context.write(word, new IntWritable(sum));
 	}
